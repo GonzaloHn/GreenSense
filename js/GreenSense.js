540931b7c64858e5b9557992e82d48bfd0c444e3
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-08-05 11:14:11 -0300

    Avances registro (back)
    arregle cosas y se puede insertar a la tabla pero no bien, tengo que arreglar lo que se inserta

--- js/GreenSense.js
+++ js/GreenSense.js
@@ -1,30 +1,34 @@
 $(document).ready(function(){
 
     $("#S").click(function(){
-        if (validar()){
-            $.ajax({
-                  type: 'POST',
-                  url: '../php/registrar.php',
-                  dataType: "json",
-                  data: 'usu=' + $("#u").val() + 'pass='+ $("#c").val() + 'ema='+$("#m").val(),
-                  success: function (datos) {
-                        if(datos.status == 'ok'){
-                            window.location.assign("../html/index.html");
-                        }else if (datos.status=='err'){
-                            mensaje="Usuario ya existente";
-                               $("#divt").html(mensaje);
-                               $("#divt").show();
-                        }else{
-                            mensaje="Ocurrio un error";
-                               $("#divt").html(mensaje);
-                               $("#divt").show();
-                        }
-                    },
-                  error: function(error) {
-                        ;
-                       },
-                });
-            }
+        $.ajax({
+              type: 'POST',
+              url: '../php/registrar.php',
+              dataType: "json",
+              data: 'usu=' + $("#u").val() + 'pass='+ $("#p").val() + 'ema='+$("#e").val(),
+              success: function (datos) {
+                    if(datos.status == 'ok')
+                    {
+                        window.location.assign("../html/index.html");
+                    }
+                    else if (datos.status == 'err')
+                    {
+                        mensaje="Usuario y contraseña existente";
+                        $("#divt").html(mensaje);
+                        $("#divt").show();
+                    }
+                    else
+                    {
+                        mensaje="Ocurrio un error";
+                        $("#divt").html(mensaje);
+                        $("#divt").show();
+                    }
+                },
+              error: function(error) {
+                    ;
+                   },
+            });
+
     });
 
 

350e0d38ad53bdc7ff6edabe1f76d8b171004be3
GonzaloHn <101721224+GonzaloHn@users.noreply.github.com>
2022-08-19 10:41:16 -0300

    Arreglando como se dibujan los gráficos
    Cuando se cambiaba de un gráfico a otro, se dibujaba mal y no tenían las "medidas" correctas. Se arreglo haciendo que se construya el gráfico cada vez que se toca el botón para cambiar de gráfico

--- js/GreenSense.js
+++ js/GreenSense.js
@@ -42,10 +42,57 @@ $(document).ready(function(){
         if ($("#chart-energy").is(":visible")){
             $("#chart-energy").hide();
             $("#chart-air").show();
+            $("#chart-air").empty();
+            $("#chart-air svg").remove();
+            new Morris.Line({
+                // ID of the element in which to draw the chart.
+                element: 'chart-air',
+                // Chart data records -- each entry in this array corresponds to a point on
+                // the chart.
+                data: [
+                  { year: '2008', value: 4 },
+                  { year: '2009', value: 4 },
+                  { year: '2010', value: 5 },
+                  { year: '2011', value: 10 },
+                  { year: '2012', value: 9 }
+                ],
+                // The name of the data record attribute that contains x-values.
+                xkey: 'year',
+                // A list of names of data record attributes that contain y-values.
+                ykeys: ['value'],
+                // Labels for the ykeys -- will be displayed when you hover over the
+                // chart.
+                labels: ['Value'],
+                resize: true,
+                redraw: true
+            });
         }
         else if ($("#chart-air").is(":visible")){
             $("#chart-air").hide();
             $("#chart-trash").show();
+            $("#chart-trash").empty();
+            $("#chart-trash svg").remove();
+            new Morris.Line({
+                // ID of the element in which to draw the chart.
+                element: 'chart-trash',
+                // Chart data records -- each entry in this array corresponds to a point on
+                // the chart.
+                data: [
+                  { year: '2008', value: 5 },
+                  { year: '2009', value: 10 },
+                  { year: '2010', value: 30 },
+                  { year: '2011', value: 50 },
+                  { year: '2012', value: 20 }
+                ],
+                // The name of the data record attribute that contains x-values.
+                xkey: 'year',
+                // A list of names of data record attributes that contain y-values.
+                ykeys: ['value'],
+                // Labels for the ykeys -- will be displayed when you hover over the
+                // chart.
+                labels: ['Value'],
+                resize: true
+            });
         }
         else if ($("#chart-trash").is(":visible")){
             $("#chart-trash").hide();

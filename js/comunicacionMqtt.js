694bd770af3a1c0f8a651b813ebdac09965664a0
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-08-22 09:11:05 -0300

    comienzo de guardar valores mqtt

--- js/comunicacionMqtt.js
+++ js/comunicacionMqtt.js
@@ -1,8 +1,32 @@
 //poner nombre de topicos como: GreenSense/Topico, asi no se repite con topicos de otras personas 
 //en ese caso para suscribirte a todos nuestros topicos puedo poner: GreenSense/# 
 
+let date = new Date();
+let time = new Date();
+
 const mqtt = require('mqtt');
 const client = mqtt.connect('mqtt://test.mosquitto.org'); //poner pagina web broker
+
+const mysql = require ('mysql');
+const conexion  = mysql.createConnection({
+            
+    host: 'localhost',
+    database: 'greensense',
+    user: 'root',
+    password: 'rootroot'
+
+});
+
+conexion.connect(function(error){
+
+    if (error){
+        throw error;
+    }
+    else{
+        console.log('conexion exitosa');
+    }
+
+});
 
 function EventoConectar(){
     client.subscribe('topicoEscuchar', function (err) {
@@ -18,9 +42,25 @@ function EventoMensaje(topic, message){
 
     if (topic == "GreenSense/basura")
     {
+        //guardar valor y subirlo al grafico
+        let fecha = date.toLocaleDateString();
+        let hora = time.toLocaleTimeString();
+        let basura = message.toString();
+
         //insertar a DB
+        conexion.query(`INSERT INTO basura (fecha,hora,basura) VALUES (${fecha} ,${hora} ,${basura} )`, function (error,results,fields){
+            if (error)
+            throw error;
+
+            results.forEach(result => {
+                console.log(result);
+            });
+        });
+
+        
     }
     
+    conexion.end();
     //client.end()
 }
 

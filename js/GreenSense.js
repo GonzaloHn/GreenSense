a30fd6bf46ce71df1e44ca957910852a7a0e1da8
GonzaloHn <101721224+GonzaloHn@users.noreply.github.com>
2022-08-26 10:25:22 -0300

    Cambio de gráficos de morris.js por chart.js
    Se cambiaron los tres gráficos de línea de la home inicial que estaban hechos con morris.js por unos con chart.js. Los tres se cambian unos por otros con un botón, solo mostrándose uno de los tres gráficos a la vez.

--- js/GreenSense.js
+++ js/GreenSense.js
@@ -34,71 +34,10 @@ $(document).ready(function(){
     //FRONT-END
     //Solo un gráfico a la vez
 
-    $("#chart-energy").show();
+    //$("#chart-energy").show();
     //$("#chart-air").hide();
     //$("#chart-trash").hide();
 
-    $("#change-g").click(function(){
-        if ($("#chart-energy").is(":visible")){
-            $("#chart-energy").hide();
-            $("#chart-air").show();
-            $("#chart-air").empty();
-            $("#chart-air svg").remove();
-            new Morris.Line({
-                // ID of the element in which to draw the chart.
-                element: 'chart-air',
-                // Chart data records -- each entry in this array corresponds to a point on
-                // the chart.
-                data: [
-                  { year: '2008', value: 4 },
-                  { year: '2009', value: 4 },
-                  { year: '2010', value: 5 },
-                  { year: '2011', value: 10 },
-                  { year: '2012', value: 9 }
-                ],
-                // The name of the data record attribute that contains x-values.
-                xkey: 'year',
-                // A list of names of data record attributes that contain y-values.
-                ykeys: ['value'],
-                // Labels for the ykeys -- will be displayed when you hover over the
-                // chart.
-                labels: ['Value'],
-                resize: true,
-                redraw: true
-            });
-        }
-        else if ($("#chart-air").is(":visible")){
-            $("#chart-air").hide();
-            $("#chart-trash").show();
-            $("#chart-trash").empty();
-            $("#chart-trash svg").remove();
-            new Morris.Line({
-                // ID of the element in which to draw the chart.
-                element: 'chart-trash',
-                // Chart data records -- each entry in this array corresponds to a point on
-                // the chart.
-                data: [
-                  { year: '2008', value: 5 },
-                  { year: '2009', value: 10 },
-                  { year: '2010', value: 30 },
-                  { year: '2011', value: 50 },
-                  { year: '2012', value: 20 }
-                ],
-                // The name of the data record attribute that contains x-values.
-                xkey: 'year',
-                // A list of names of data record attributes that contain y-values.
-                ykeys: ['value'],
-                // Labels for the ykeys -- will be displayed when you hover over the
-                // chart.
-                labels: ['Value'],
-                resize: true
-            });
-        }
-        else if ($("#chart-trash").is(":visible")){
-            $("#chart-trash").hide();
-            $("#chart-energy").show();
-        }
-    });
 
     $("#fR").click(function(){
         $("body").css("background-color","#FF0000");
@@ -154,6 +93,14 @@ $(document).ready(function(){
             }
         }
     });
+
+    $("#hide-g").click(function(){
+        if ($("#show-g").is(":visible")){
+        $("#show-g").hide();
+        }else if($("#show-g").is(":hidden")){
+            $("#show-g").show();
+        }
+    });
     const cx = $('#myCHart');
     const myChart2 = new Chart(cx, {
         type: 'doughnut',
@@ -170,16 +117,21 @@ $(document).ready(function(){
                   'rgb(0, 0, 0, 0)',
                 ],
                 hoverOffset: 4,
+                borderWidth: 1
             }]
         },
         options: {
             events: ["", "mouseout", "", "touchstart", "touchmove", "touchend"],
-            cutout:150,
+            cutout:170,
             plugins: {
                 legend: {
                   display: false
                 }
             },
+        },
+        centerText: {
+            display: true,
+            text: "280"
         }
     });
 });
---
+++ js/home-graphics.js
@@ -0,0 +1,100 @@
+$(document).ready(function(){
+const line_energy = $('#line-chart-energy');
+const line_chart_energy = new Chart(line_energy, {
+    type: 'line',
+    data: {
+        labels: ["a", "b", "c", "d", "e", "f", "g"],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(241, 198, 7)',
+            tension: 0.1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+    },
+});
+
+const line_air = $('#line-chart-air');
+const line_chart_air = new Chart(line_air, {
+    type: 'line',
+    data: {
+        labels: ["aa", "bb", "cc", "dd", "ee", "ff", "gg"],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [70, 13, 1, 1, 1, 100, 30],
+            fill: false,
+            borderColor: 'rgb(51, 255, 215)',
+            tension: 0.1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+    },
+});
+
+const line_trash = $('#line-chart-trash');
+const line_chart_trash = new Chart(line_trash, {
+    type: 'line',
+    data: {
+        labels: ["aaa", "bbb", "ccc", "ddd", "eee", "fff", "ggg"],
+        datasets: [{
+            label: 'My First Dataset',
+            data: [30, 50, 89, 69, 10, 0, 40],
+            fill: false,
+            borderColor: 'rgb(241, 7, 7)',
+            tension: 0.1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        },
+    },
+});
+$("#hide-a-g").hide();
+$("#hide-t-g").hide();
+
+$("#change-g").click(function(){
+    if ($("#hide-e-g").is(":visible")){
+        $("#hide-e-g").hide();
+        $("#hide-a-g").show();
+        $("#hide-a-g").removeClass("none");
+    }
+    else if ($("#hide-a-g").is(":visible")){
+        $("#hide-a-g").hide();
+        $("#hide-t-g").show();
+    }
+    else if ($("#hide-t-g").is(":visible")){
+        $("#hide-t-g").hide();
+        $("#hide-e-g").show();
+    }
+});
+});

225f962caa7b4a779886bf6ea30497f2e0b105fa
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-08-26 11:37:47 -0300

    Comienzo de registro bien
    Registro comunicado con html pero con puerto 3000. Tengo que comunicarlo con pagina normal y hacer andar comunicacion con DB (que sea non-blocking me complica un poco las cosas)

---
+++ js/registro.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const mysql = require ('mysql');
+
+let user = "";
+let pass = "";
+let email = "";
+
+const app = express();
+
+app.use (express.urlencoded({extended: true})); //decodificar daa que pasa html
+
+app.get ('/', (req, res) => {
+    res.sendFile('C:\\AppServ\\www\\GreenSense\\html\\registrar.html');
+});
+
+app.post('/', (req,res)=>{
+    
+    user = req.body.usuario;
+    pass = req.body.contra;
+    email = req.body.mail;
+
+    /*
+    if (user == "" || pass == "" || email == "")
+    {
+        res.status(400).send("debde ingresar todos los valores");
+    }
+    */
+
+    //anda pero saca datos de replica de pagina (localhost:3000, en vez del appserv, es decir la ag principal)
+   
+});
+
+const conexion  = mysql.createConnection({
+    
+    host: 'localhost',
+    database: 'greensense',
+    user: 'root',
+    password: 'rootroot'
+
+});
+
+conexion.connect(function(error){
+
+    if (error){
+        throw error;
+    }
+    else{
+        console.log('conexion exitosa');
+    }
+
+});
+
+/*
+conexion.query("INSERT INTO usuarios (usuario, contrasenia, gmail) VALUES ('a' ,'b' ,'c')", function (error,results,fields){
+    if (error)
+        throw error;
+            
+    });
+    
+    conexion.end();
+*/
+
+
+if (user == "" || pass == "" || email == "")
+{
+    console.log ("error, faltan datos de registro")
+}
+else 
+{
+    conexion.query('INSERT INTO usuarios (usuario, contrasenia, gmail) VALUES ("'+user+'" ,"'+pass+'" ,"'+email+'" )', function (error,results,fields){
+        if (error) throw error;
+    });
+    
+    conexion.end();
+}
+
+
+/*
+app.get ('/styles.css', (req, res) => {
+    res.sendFile('C:\\AppServ\\www\\GreenSense\\css\\syles.css');
+})
+*/
+
+app.listen (3000);
+console.log ('Server on port 3000')

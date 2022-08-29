4789e06118f194af20baf3acb01c152c087c9f01
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-08-29 11:31:29 -0300

    Archivos de mas eliminados

--- js/GreenSense.js
+++ js/GreenSense.js
@@ -1,36 +1,5 @@
 $(document).ready(function(){
 
-    $("#S").click(function(){
-        $.ajax({
-              type: 'POST',
-              url: '../php/registrar.php',
-              dataType: "json",
-              data: 'usu=' + $("#u").val() + '&pass='+ $("#c").val() + '&ema='+ $("#m").val(),
-              success: function (datos) {
-                    if(datos.status == 'ok')
-                    {
-                        window.location.assign("../html/index.html");
-                    }
-                    else if (datos.status == 'err')
-                    {
-                        mensaje="Usuario y contraseña existente";
-                        $("#divt").html(mensaje);
-                        $("#divt").show();
-                    }
-                    else
-                    {
-                        mensaje="Ocurrio un error";
-                        $("#divt").html(mensaje);
-                        $("#divt").show();
-                    }
-                },
-              error: function(error) {
-                    
-                   },
-            });
-
-    });
-
     //FRONT-END
     //Solo un gráfico a la vez
 

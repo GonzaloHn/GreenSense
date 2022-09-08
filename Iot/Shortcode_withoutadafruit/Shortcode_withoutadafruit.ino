c4c5969b5808990867d1fb41722c27ad261a8d87
soficasares <102155698+soficasares@users.noreply.github.com>
2022-09-08 09:45:47 -0300

    Short Code for MQ135

---
+++ Iot/Shortcode_withoutadafruit/Shortcode_withoutadafruit.ino
@@ -0,0 +1,18 @@
+#include <MQ135.h>
+#include <ESP8266WiFi.h>
+
+void setup() {
+Serial.begin(9600); //Init serial port
+  delay(200);
+
+}
+
+void loop() {
+   MQ135 gasSensor = MQ135(A0);
+    float air_quality = gasSensor.getPPM();
+    Serial.print("Air Quality: ");  
+    Serial.print(air_quality);
+    Serial.println("  PPM");   
+     Serial.println();
+     delay(500);
+}  

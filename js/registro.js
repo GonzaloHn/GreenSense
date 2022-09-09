b6a0e95226074b0b5ebc286e281594b64a089b6b
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-09-09 14:33:04 -0300

    tirar error al registrar usaurio existente

--- js/registro.js
+++ js/registro.js
@@ -27,31 +27,37 @@ app.post('/', (req,res)=>{
     pass = req.body.contra;
     email = req.body.mail;
 
-    //Tirar error si faltan valores (no anda bien)
-    if (user == "" || pass == "" || email == "")
-    {
-        //tira error enpuerto 3000 (deberia redirijrlo al front despues)
-        res.status(400).send("debe ingresar todos los valores");
-        console.log ("error, faltan datos de registro")
-    }
+    conexion.query('SELECT usuario FROM usuarios WHERE usuario= ?', [user], function (err, row) {
+        //tira error si el usuario ya existe
+        if (row && row.length) {
+            console.log("Error: el usuario ya existe");
+            res.status(400).send("Error: El nombre de usuario ya existe");
+        }
+        //tira error si faltan valores (no anda bien)
+        else if (user == "" || pass == "" || email == "")
+        {
+            //tira error enpuerto 3000 (deberia redirijrlo al front despues)
+            res.status(400).send("Error: Debe ingresar todos los valores");
+            console.log ("error, faltan datos de registro")
+        }
+        //Insertar regisistro a DB
+        else 
+        {
+            conexion.query('INSERT INTO usuarios (usuario, contrasenia, gmail) VALUES ("'+user+'" ,"'+pass+'" ,"'+email+'" )', function (error,results,fields){
+                if (error) throw error;
+                console.log ("registro insertado");
+                
+                //Quiero redirigir al front luego de crear registro
 
-    //Insertar regisistro a DB
-    else 
-    {
-        conexion.query('INSERT INTO usuarios (usuario, contrasenia, gmail) VALUES ("'+user+'" ,"'+pass+'" ,"'+email+'" )', function (error,results,fields){
-            if (error) throw error;
-            console.log ("registro insertado");
+                //me redirije a puerto 3000 en vez de puerto mio
+                res.redirect("http://localhost/GreenSense/html");
+                //conexion.end();
+            });
             
-            //Quiero redirigir al front luego de crear registro
+            
+        }
+    });
 
-            //me redirije a puerto 3000 en vez de puerto mio
-            res.redirect("http://localhost/GreenSense/html");
-            //conexion.end();
-        });
-        
-        
-    }
-   
 });
 
 /*
@@ -61,4 +67,4 @@ app.get ('/styles.css', (req, res) => {
 */
 
 app.listen (3000);
-console.log ('Servidor en puerto 3000, escuchando registro...')
+console.log ('Servidor en puerto 3000, escuchando registro...');

244fb2a6c7ee9c88c02186fab877b46144e6c852
GonzaloHn <101721224+GonzaloHn@users.noreply.github.com>
2022-09-12 12:03:55 -0300

    DyGraph Prueba

---
+++ js/prueba.js
@@ -0,0 +1,21 @@
+graph = new Dygraph(
+    document.getElementById("graph"),
+
+    "Year ,Fatalities\n" +
+    "2008, 200\n" +
+    "2009, 300\n" +
+    "2010, 200\n" +
+    "2011, 100\n" +
+    "2012, 500\n" +
+    "2012.4, 300\n" +
+    "2013, 200\n" +
+    "2014, 500\n" +
+    "2015, 300\n" +
+    "2016, 800\n" +
+    "2017, 600\n",
+
+    {
+        animatedZooms: true
+    }
+)
+

c182e5121f5b197026aa18a49f634e14dc66b27e
GonzaloHn <101721224+GonzaloHn@users.noreply.github.com>
2022-09-16 10:37:32 -0300

    Recarga dinámica DyGraph

--- js/GreenSense.js
+++ js/GreenSense.js
@@ -15,86 +15,11 @@ $(document).ready(function(){
     });
 
 
-    //Probando chart.js "En progreso"
-    const ctx = $('#myChart');
-    const myChart = new Chart(ctx, {
-        type: 'bar',
-        data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true
-                }
-            },
-            plugins: {
-                legend: {
-                  display: false
-                }
-            }
-        }
-    });
-
     $("#hide-g").click(function(){
         if ($("#show-g").is(":visible")){
         $("#show-g").hide();
         }else if($("#show-g").is(":hidden")){
             $("#show-g").show();
-        }
-    });
-    const cx = $('#myCHart');
-    const myChart2 = new Chart(cx, {
-        type: 'doughnut',
-        data: {
-            labels: [
-                'Green',
-                'Blue',
-              ],
-              datasets: [{
-                label: 'My First Dataset',
-                data: [200, 150],
-                backgroundColor: [
-                  'rgb(138,221,45)',
-                  'rgb(0, 0, 0, 0)',
-                ],
-                hoverOffset: 4,
-                borderWidth: 1
-            }]
-        },
-        options: {
-            events: ["", "mouseout", "", "touchstart", "touchmove", "touchend"],
-            cutout:170,
-            plugins: {
-                legend: {
-                  display: false
-                }
-            },
-        },
-        centerText: {
-            display: true,
-            text: "280"
         }
     });
 });
--- js/prueba.js
+++ js/prueba.js
@@ -1,21 +1,38 @@
-graph = new Dygraph(
-    document.getElementById("graph"),
+// graph = new Dygraph(
+//     document.getElementById("graph"),
 
-    "Year ,Fatalities\n" +
-    "2008, 200\n" +
-    "2009, 300\n" +
-    "2010, 200\n" +
-    "2011, 100\n" +
-    "2012, 500\n" +
-    "2012.4, 300\n" +
-    "2013, 200\n" +
-    "2014, 500\n" +
-    "2015, 300\n" +
-    "2016, 800\n" +
-    "2017, 600\n",
+//     "Year ,Fatalities\n" +
+//     "2008, 200\n" +
+//     "2009, 300\n" +
+//     "2010, 200\n" +
+//     "2011, 100\n" +
+//     "2012, 500\n" +
+//     "2012.4, 300\n" +
+//     "2013, 200\n" +
+//     "2014, 500\n" +
+//     "2015, 900\n" +
+//     "2016, 400\n" +
+//     "2017, 600\n",
 
-    {
-        animatedZooms: true
-    }
-)
+//     {
+//         animatedZooms: true,
+        
+//         errorBars: true,
+//     },
+// )
+var data = [];
+var t = new Date();
+for (var i = 10; i >= 0; i--) {
+    var x = new Date(t.getTime() - i * 1000);
+    data.push([x, Math.random()]);
+} // todo esto sirve para agarrar el momento actual y guardarlo en el data de arriba
 
+var g = new Dygraph(document.getElementById("graph"), data, {width:500, color:"yellow"}); //crea el nuevo grafico
+
+window.setInterval(function() {
+        var x = new Date(); 
+        var y = Math.random(); //agarra un valor random para Y
+        data.shift(); //remueve el primer dato del array, adelantando a todos los demas por uno adelante
+        data.push([x, y]); //añade un nuevo elemtno al final de un array
+        g.updateOptions( { 'file': data } ); //actualiza el grafico con nuevos valores, los de data
+      }, 1000);

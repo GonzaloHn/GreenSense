7ecb62fdc5e271186482b60bb1616ce5514683a2
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-10-13 11:05:36 -0300

    mail de borrado (no anda)
    tengo que ver como agarrar el mail de la DB y mandar mail al borrar usuario

--- js/back/borrar.js
+++ js/back/borrar.js
@@ -6,9 +6,16 @@ const port = 3000;
 //Variables mysql
 const mysql = require ('mysql');
 
+//Variables nodemailer
+//El sender de mail es desde ethereal
+const nodemailer = require ('nodemailer');
+let transporter;
+let mailOptions;
+
 //Variables de usuario 
 let user = "";
 let pass = "";
+let email = "";
 
 //Conexion DB
 const conexion  = mysql.createConnection({
@@ -36,9 +43,42 @@ app.post('/', (req,res)=>{
             
             //Se borra usuario si todos los datos cumplen requerimientos basicos
             if (results.length > 0) {
+
+                conexion.query('SELECT gmail FROM usuarios WHERE usuario = ? AND contrasenia = ?' [user, pass], function (err, result) {
+                    email = result;
+                })
+
                 conexion.query('DELETE FROM usuarios WHERE usuario = ? AND contrasenia = ?', [user, pass]);
                 console.log("> usuario borrado");
                 res.redirect("http://localhost/GreenSense/html");
+
+                transporter = nodemailer.createTransport({
+                    host: "smtp.ethereal.email",
+                    port: 587,
+                    secure: false,
+                    auth: {
+                        user: "dakota64@ethereal.email",
+                        pass: "HyxQd9KvW1ryFUnqhs",
+                    },
+                });
+
+                mailOptions = {
+                    from: "Remitente",
+                    to: email,
+                    subject: "Cuenta borrada en Green Sense",
+                    text: "Su cuenta de Green Sense ha sido borrada exitosamente.",
+                };
+                
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        console.log ("> error enviando mail de borrado de cuenta");
+                        //res.status(400).send("Error: El mail de registración no ha podido ser enviado");   
+                    }
+                    else {
+                        console.log ("> mail de borrado de cuenta enviado");
+                    }
+                });
+
             }
 
             //Tira error si el usuario o contraseña esta mal

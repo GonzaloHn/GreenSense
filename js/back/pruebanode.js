ba3eb26fd9967fee44f0153e22def0f19b6e700a
Thiago Orué <89083464+Disrupt2006@users.noreply.github.com>
2022-10-18 15:27:19 -0300

    pruebas de seleccionar todos los mails (para mandar mail a todos los usuarios)

--- js/back/pruebanode.js
+++ js/back/pruebanode.js
@@ -13,7 +13,7 @@ async function init() {
 
  init();
  */
-
+/*
  const express = require('express');
  const app = express();
  const port = 9000;
@@ -27,27 +27,29 @@ async function init() {
     console.log ('dato de basura enviado');
     socket.emit('basura', basura);
   });
-
+*/
   //Mandar valores a front (a todos los clientes)
   /*
   console.log ('dato de basura enviado');
   io.emit('basura', basura);
   */
 
-  //escucha a puerto 9000
-/*
-app.listen (port, () => {
-  console.log (`Servidor en puerto ${port}, escuchando registro...`);
+ // .---------------------------------------------------------------------------------
+
+ const mysql = require ('mysql');
+
+ const conexion  = mysql.createConnection({
+    
+  host: 'localhost',
+  database: 'greensense',
+  user: 'root',
+  password: 'rootroot'
+
 });
-*/
 
- /*
-   io.on('basura', basura);
-  console.log ('dato de basura enviado');
-*/
+conexion.query("SELECT gmail FROM usuarios", function (error,result,fields){
+  if (error)throw error;
+  let mails = [result];
+  console.log(mails);  
+});   
 
- 
-
-
-
- 

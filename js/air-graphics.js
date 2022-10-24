760f33248d47bd521b3fcd23f7f03790705df05a
GonzaloHn <101721224+GonzaloHn@users.noreply.github.com>
2022-10-24 13:34:21 -0300

    Texto dentro del chart 2

--- js/air-graphics.js
+++ js/air-graphics.js
@@ -1,38 +1,77 @@
-$(document).ready(function(){
+// setup 
+const data = {
+    labels: [
+      'Green',
+      'Blue',
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [60, 40],
+      borderColor: [
+          'rgb(241, 198, 7)',
+          'rgb(0, 0, 0, 0)',
+      ],
+      backgroundColor: [
+        'rgb(241, 198, 7)',
+        'rgb(0, 0, 0, 0)',
+      ],
+      hoverOffset: 4,
+      borderWidth: 1
+  }]
+};
+  
+//centerText plugin
+const centerText = {
+    id: 'centerText',
+    afterDatasetsDraw(chart, args, options) {
+      const { ctx, chartArea: {left, right, top, bottom, width, height} } = chart;
+  
+      ctx.save();
+      console.log(top);
+  
+      ctx.font = 'bolder 25px Arial';
+      ctx.fillStyle = 'rgba(1, 1, 1)';
+      ctx.textAlign = 'center';
+      ctx.fillText('Energía', width / 2, height / 2 - 60);
+      ctx.restore();
+  
+      ctx.font = 'bolder 25px Arial';
+      ctx.fillStyle = 'rgba(1, 1, 1)';
+      ctx.textAlign = 'center';
+      ctx.fillText('electrica', width / 2, height / 2 - 30);
+      ctx.restore();
 
-    const donut_air = $('#donut-air-energy');
-    const donut_chart_air = new Chart(donut_air, {
-        type: 'doughnut',
-        data: {
-            labels: [
-                'Green',
-                'Blue',
-              ],
-              datasets: [{
-                label: 'My First Dataset',
-                data: [200, 150],
-                backgroundColor: [
-                  'rgb(51, 255, 215)',
-                  'rgb(0, 0, 0, 0)',
-                ],
-                hoverOffset: 4,
-                borderWidth: 1
-            }]
+      const val_actual = 'Valor actual: numero';
+
+      ctx.font = 'bolder 30px Arial';
+      ctx.fillStyle = 'rgba(1, 1, 1)';
+      ctx.textAlign = 'center';
+      ctx.fillText(val_actual, width / 2, height / 2 + top);
+      ctx.restore();
+    }
+  }
+  
+  // config 
+  const config = {
+    type: 'doughnut',
+    data,
+    options: {
+      responsive: true,
+      events: ["", "mouseout", "", "", "touchmove", "touchend"],
+      cutout:'90%',
+      plugins: {
+        legend: {
+           display: false
         },
-        options: {
-            events: ["", "mouseout", "", "", "touchmove", "touchend"],
-            cutout:'90%',
-            responsive: true,
-            maintainAspectRatio: true, 
-            plugins: {
-                legend: {
-                  display: false
-                }
-            },
-        },
-        centerText: {
-            display: true,
-            text: "280"
-        }
-    });
-    });
+    }
+    }, 
+    plugins: [
+      centerText
+    ]
+};
+  
+  // render init block
+const myChart = new Chart(
+    document.getElementById('donut-chart-air'),
+    config
+);
--- js/weight-graphics.js
+++ js/weight-graphics.js
@@ -1,80 +1,77 @@
-$(document).ready(function(){
-    // const line_energy = $('#line-chart-energy');
-    // const line_chart_energy = new Chart(line_energy, {
-    //     type: 'line',
-    //     data: {
-    //         labels: ["a", "b", "c", "d", "e", "f", "g"],
-    //         datasets: [{
-    //             label: 'My First Dataset',
-    //             data: [65, 59, 80, 81, 56, 55, 40],
-    //             fill: false,
-    //             borderColor: 'rgb(241, 198, 7)',
-    //             tension: 0.1
-    //         }]
-    //     },
-    //     options: {
-    //         scales: {
-    //             y: {
-    //                 beginAtZero: true
-    //             }
-    //         },
-    //         plugins: {
-    //             legend: {
-    //                 display: false
-    //             }
-    //         },
-    //     },
-    // });
-    
-    const donut_weight = $('#donut-chart-weight');
-    const donut_chart_weight = new Chart(donut_weight, {
-        type: 'doughnut',
-        data: {
-            labels: [
-                'Green',
-                'Blue',
-              ],
-              datasets: [{
-                label: 'My First Dataset',
-                data: [200, 150],
-                backgroundColor: [
-                  'rgb(241, 7, 7)',
-                  'rgb(0, 0, 0, 0)',
-                ],
-                hoverOffset: 4,
-                borderWidth: 1
-            }]
+// setup 
+const data = {
+    labels: [
+      'Green',
+      'Blue',
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [60, 40],
+      borderColor: [
+          'rgb(241, 198, 7)',
+          'rgb(0, 0, 0, 0)',
+      ],
+      backgroundColor: [
+        'rgb(241, 198, 7)',
+        'rgb(0, 0, 0, 0)',
+      ],
+      hoverOffset: 4,
+      borderWidth: 1
+  }]
+};
+  
+//centerText plugin
+const centerText = {
+    id: 'centerText',
+    afterDatasetsDraw(chart, args, options) {
+      const { ctx, chartArea: {left, right, top, bottom, width, height} } = chart;
+  
+      ctx.save();
+      console.log(top);
+  
+      ctx.font = 'bolder 25px Arial';
+      ctx.fillStyle = 'rgba(1, 1, 1)';
+      ctx.textAlign = 'center';
+      ctx.fillText('Energía', width / 2, height / 2 - 60);
+      ctx.restore();
+  
+      ctx.font = 'bolder 25px Arial';
+      ctx.fillStyle = 'rgba(1, 1, 1)';
+      ctx.textAlign = 'center';
+      ctx.fillText('electrica', width / 2, height / 2 - 30);
+      ctx.restore();
+
+      const val_actual = 'Valor actual: numero';
+
+      ctx.font = 'bolder 30px Arial';
+      ctx.fillStyle = 'rgba(1, 1, 1)';
+      ctx.textAlign = 'center';
+      ctx.fillText(val_actual, width / 2, height / 2 + top);
+      ctx.restore();
+    }
+  }
+  
+  // config 
+  const config = {
+    type: 'doughnut',
+    data,
+    options: {
+      responsive: true,
+      events: ["", "mouseout", "", "", "touchmove", "touchend"],
+      cutout:'90%',
+      plugins: {
+        legend: {
+           display: false
         },
-        options: {
-            events: ["", "mouseout", "", "", "", "touchend"],
-            cutout:'90%',
-            plugins: {
-                legend: {
-                  display: false
-                }
-            },
-        },
-        centerText: {
-            display: true,
-            text: "280"
-        }
-    });
-    // $("#hide-a-g").hide();
-    // $("#hide-t-g").hide();
-    
-    // $("#change-g").click(function(){
-    //     if ($("#hide-e-g").is(":visible")){
-    //         $("#hide-e-g").hide();
-    //         $("#hide-a-g").show();
-    //         $("#hide-a-g").removeClass("none");
-    //     }
-    //     else if ($("#hide-a-g").is(":visible")){
-    //         $("#hide-a-g").hide();
-    //         $("#hide-t-g").show();
-    //     }
-    //     else if ($("#hide-t-g").is(":visible")){
-    //         $("#hide-t-g").hide();
-    //         $("#hide-e-g").show();
-    //     }
-    // });
-    });
+    }
+    }, 
+    plugins: [
+      centerText
+    ]
+};
+  
+  // render init block
+const myChart = new Chart(
+    document.getElementById('donut-chart-weight'),
+    config
+);
